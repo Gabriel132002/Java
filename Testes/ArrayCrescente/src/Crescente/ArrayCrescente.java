7b9d655af104b786faea4399c53832d1f6c8bda5
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-09-09 11:00:32 -0300

    java commit

--- Testes/ArrayCrescente/src/Crescente/ArrayCrescente.java
+++ Testes/ArrayCrescente/src/Crescente/ArrayCrescente.java
@@ -1,11 +1,16 @@
 package Crescente;
 
+import java.util.Scanner;
+
 public class ArrayCrescente {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Sou Gabriel");
-        System.out.println("Testando mais vezes");
+       int x;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite qualquer valor");
+        x = in.nextInt();
+        System.out.println(x);
     }
 }
